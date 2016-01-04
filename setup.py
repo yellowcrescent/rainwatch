d5c852f02ea5c626cef1491e8e3c4fa17282279f
jacob <jacob@ycnrg.org>
2016-01-03 19:35:53 -0500

    troubleshooting scp fuckery

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "rainwatch",
-    version = "0.10.3",
+    version = "0.10.4",
     author = "Jacob Hipps",
     author_email = "jacob@ycnrg.org",
     license = "MIT",
